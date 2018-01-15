a60c896ffef64d68a93f293a191b8c26baacfa80
Paul Connolly <paul.connolly@npd.com>
2018-01-15 12:47:30 +0000

    expanding config download section

--- dlpziplib/Config.cs
+++ dlpziplib/Config.cs
@@ -5,11 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Win32; // for registry
 using System.Diagnostics; // for trace output
+using System.IO; // for file copies
+using System.Net; //for url copies
 
 namespace dlpziplib
 {
     public class Config
     {
+        private const string localConfigFile = "DLPZip.config";
+
         public static void ReadConfig()
         {
             Trace.WriteLine("DLPZip.exe: ReadConfig()");
@@ -19,27 +23,74 @@ namespace dlpziplib
             const string subkey = "SOFTWARE\\DLPZip";
             const string keyName = userRoot + "\\" + subkey;
 
+            
+
             try
             {
                 var regval = (long)Registry.GetValue(keyName, "NCTS", 0);
+                Trace.WriteLine("DLPZip.exe: Read " + regval.ToString() + " from " + keyName);
                 ncts = DateTime.FromBinary(regval);
+                Trace.WriteLine("DLPZip.exe: Parsed ncts as " + ncts.ToString());
             }
             catch (NullReferenceException)
             { 
                 ncts = DateTime.MinValue;
+                Trace.WriteLine("DLPZip.exe: " + keyName + " not read, using " + ncts.ToString());
             }
 
             if (DateTime.Now >= ncts)
             {
                 Trace.WriteLine("DLPZip.exe: Re-reading config");
+                String configSource;
 
+                try
+                {
+                    var regval = (String)Registry.GetValue(keyName, "ConfigLocation", "");
+                    Trace.WriteLine("DLPZip.exe: Read '" + regval.ToString() + "' from " + keyName);
+                    if (regval.Length == 0)
+                        throw new ArgumentNullException("ConfigLocation not defined");
+                    configSource = regval.ToString();
+                }
+                catch (NullReferenceException)
+                {
+                    Trace.WriteLine("DLPZip.exe: " + keyName + " not read, using ConfigLocation");
+                    throw new ArgumentNullException("ConfigLocation");
+                }
+
+                if (configSource.StartsWith("http"))
+                {
+                    Trace.WriteLine("DLPZip.exe: ConfigLocation is a URL");
+
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(configSource, localConfigFile);
+                    }
+                }
+                else
+                {
+                    Trace.WriteLine("DLPZip.exe: Assuming ConfigLocation is a path");
+                    File.Copy(configSource, localConfigFile, true);
+                }
                 //TODO write time + delay to registry
                 Registry.SetValue(keyName, "NCTS", DateTime.Now.ToBinary(), RegistryValueKind.QWord);
             }
+            else
+            {
+                Trace.WriteLine("Not re-reading config");
+            }
+
+            ParseConfig();
 
             //string json = @"{""key1"":""value1"",""key2"":""value2""}";
             //JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
             Trace.WriteLine("DLPZip.exe: ~ReadConfig()");
+        }
+
+        private static void ParseConfig()
+        {
+            //decrypt
+            Trace.WriteLine("DLPZip.exe: Config encryption not implemented");
+            return;
         }
         //foreach (String path in inputfiles)
         //{
